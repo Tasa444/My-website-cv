302fb187b52ab5be1251dd581a7515dd3ec35207
Tasa444 <149934142+Tasa444@users.noreply.github.com>
2024-10-21 23:47:44 +0200

    changed image

---
+++ main.js
@@ -0,0 +1,5 @@
+// Add the class 'slide-in' to the image when the page finishes loading
+window.addEventListener("load", function () {
+  const image = document.querySelector(".tasaImg");
+  image.classList.add("slide-in"); // Add the slide-in class to the image
+});
