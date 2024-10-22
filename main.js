2a03eb5b1cf501449ee43d0843d08e71371cc66a
Tasa444 <149934142+Tasa444@users.noreply.github.com>
2024-10-22 16:12:03 +0200

    worked on portfolio

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ window.addEventListener("load", function () {
   const image = document.querySelector(".tasaImg");
   image.classList.add("slide-in"); // Add the slide-in class to the image
 });
+
+// Function to add the slide-in effect to the logo
+function slideInLogo() {
+  // Select the logo element
+  var logo = document.querySelector(".logoContact");
+
+  // Add the 'logo-slide-in' class after the page loads
+  logo.classList.add("logo-slide-in");
+}
+
+// Wait for the DOM to fully load before executing the function
+window.addEventListener("load", slideInLogo);
